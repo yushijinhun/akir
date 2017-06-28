e7a7e18db8cf723460d061caa623e0c830ec0f10
yushijinhun <yushijinhun@gmail.com>
2017-06-28 18:12:51 +0800

    wip

--- src/main/java/org/to2mbn/akir/Application.java
+++ src/main/java/org/to2mbn/akir/Application.java
@@ -1,5 +1,9 @@
 package org.to2mbn.akir;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // TODO: to be removed
 public class Application {
 
+	private static final String CONFIG_PATH = "application.yaml";
+	private static final String DEFAULT_CONFIG_PATH = "/default-application.yaml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		if (doesConfigExist()) {
+			SpringApplication.run(Application.class, args);
+		} else {
+			if (copyConfig()) {
+				System.err.println("No configuration is detected.");
+				System.err.println("A new configuration has been written to '" + CONFIG_PATH + "'.");
+				System.err.println("Please edit it and restart akir.");
+			}
+			System.exit(1);
+		}
+	}
+
+	private static boolean doesConfigExist() {
+		return Files.exists(Paths.get(CONFIG_PATH));
+	}
+
+	private static boolean copyConfig() {
+		try (InputStream in = Application.class.getResourceAsStream(DEFAULT_CONFIG_PATH)) {
+			if (in == null) {
+				System.err.println(DEFAULT_CONFIG_PATH + " not exists");
+				return false;
+			}
+			Files.copy(in, Paths.get(CONFIG_PATH));
+			return true;
+		} catch (IOException e) {
+			System.err.println("Couldn't create default configuration");
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	// TODO: to be removed
